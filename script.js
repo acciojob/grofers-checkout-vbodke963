b8369e40788237f958f107be959028b078555f19
vbodke963 <156099787+vbodke963@users.noreply.github.com>
2026-05-13 14:01:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,9 +3,30 @@ getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
-  
+
+    // select all price elements
+    const prices = document.querySelectorAll(".prices");
+
+    let total = 0;
+
+    // calculate total
+    prices.forEach((price) => {
+        total += Number(price.innerText);
+    });
+
+    // create new row
+    const tr = document.createElement("tr");
+
+    // create new cell
+    const td = document.createElement("td");
+
+    td.colSpan = 2;
+    td.innerText = total;
+
+    tr.appendChild(td);
+
+    // append row to table
+    document.querySelector("table").appendChild(tr);
 };
 
 getSumBtn.addEventListener("click", getSum);
-
